1dd323e6f62b75c232d0b954d23a2c9b474fcca5
Ruifeng Tang <tang7h@gmail.com>
2016-03-12 15:20:44 +0800

    内容添加和功能完善

--- ynsdfx-app.js
+++ ynsdfx-app.js
@@ -1,9 +1,22 @@
 
-$(document).ready(function initial() {
-	// $('.page').hide();
-	$('#page-1').show();
+// $(document).ready(function initial() {
+// 	// $('.page').hide();
+// 	$('#page-1').show();
+// })
+// function page(index) {
+// 	$('.page').hide();
+// 	$('#page-'+index).show();
+// }
+$(document).ready(function(){
+	$('.nav-toggle').click(function(){
+		$('.nav-toggle').removeClass('curr');
+		$(this).addClass('curr');
+	});
+
+	$('.btn-page-edit').click(function(){
+		$('#page-edit').toggleClass('show');
+	});
+	$('.btn-page-edit-close').click(function(){
+		$('#page-edit').removeClass('show');
+	});
 })
-function page(index) {
-	$('.page').hide();
-	$('#page-'+index).show();
-}
