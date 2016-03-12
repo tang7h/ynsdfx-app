d5e35b95df4734244c5d118620687640b9c044ee
Ruifeng Tang <tang7h@gmail.com>
2016-03-12 17:26:12 +0800

    进一步添加细节

--- ynsdfx-app.js
+++ ynsdfx-app.js
@@ -1,12 +1,14 @@
 
-// $(document).ready(function initial() {
-// 	// $('.page').hide();
-// 	$('#page-1').show();
-// })
-// function page(index) {
-// 	$('.page').hide();
-// 	$('#page-'+index).show();
-// }
+$(document).ready(function initial() {
+	$('.page').hide();
+	$('#page-1').show();
+	$('#page-edit').show();
+})
+function page(index) {
+	$('.page').hide();
+	$('#page-edit').show();
+	$('#page-'+index).show();
+}
 $(document).ready(function(){
 	$('.nav-toggle').click(function(){
 		$('.nav-toggle').removeClass('curr');
